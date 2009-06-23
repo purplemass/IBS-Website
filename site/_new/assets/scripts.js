a402ccc0fe2df37c6de6ffa85017aea2cd013916
Babak Hatmian <babak.hatamian@imagination.com>
2009-06-24 00:16:53 +0100

    added pdf form + changed GA code + removed subscription form

--- site/_new/assets/scripts.js
+++ site/_new/assets/scripts.js
@@ -22,8 +22,6 @@ $(document).ready(function(){
 		.attr("target", "_blank");
 	$('a[href$=".pdf"]')
 		.attr("target", "_blank");
-	$('a[href$=".doc"]')
-		.attr("target", "_blank");
 	
 	// select the input box when clicked
 	$("#emailAddress").click(function(){
