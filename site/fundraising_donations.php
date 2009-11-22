e6411453970627360db41bd7e64a7a5ad7621181
Babak Hatmian <babak.hatamian@imagination.com>
2009-11-22 21:44:06 +0000

    the mixup was finally solved by replacing donations page from previous commit - how did it all get mixed up in the first place?

--- site/fundraising_donations.php
+++ site/fundraising_donations.php
@@ -1,108 +1,171 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<title>Iranian Business School Project</title>
-		
-		<link rev="start" href="./" title="Home Page" />
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-		<link rel="alternate" type="application/atom+xml" href="http://www.w3.org/QA/Tools/validator-whatsnew.atom" />
+<?php
 
-		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+#error_reporting(E_ERROR);
 
-		<meta name="keywords" content="Iranian Business School Project" />
-		<meta name="description" content="The Iranian Business School is a philanthropically motivated project aimed at creating a world-class business school in Iran. The School will offer a broad range of international standard management training programmes and courses to include EMBAs, MBAs and others." />
-		
-		<!--
-		<meta http-equiv="pragma" content="no-cache" />
-		<meta http-equiv="cache-control" content="no-cache" />
-		-->
+define('INCLUDE_CHECK', true);
 
-		<meta name="revised" content="BH 2009-05-15" />
+require 'include/db.php';
+require 'include/functions.php';
 
-		<style type="text/css" media="all">
-  			@import "./assets/styles.css";
-		</style>
-		
-		<script type="text/javascript" src="assets/jquery.js"></script>
-		<script type="text/javascript" src="assets/cufon-yui.js"></script>
-		<script type="text/javascript" src="assets/Helvetica_Neue_400.font.js"></script>
-		<script type="text/javascript" src="assets/Helvetica_Neue_500.font.js"></script>
-		<script type="text/javascript" src="assets/scripts.js"></script>
-		
-		<script type="text/javascript">
-			function CI(img)
-			{
-				$(".mainImage").attr("src", "test_images/" + img + ".jpg");
+$flag = 'start';
+$err = array();
 
-			}
-		</script>
+/////////////////////////////////////////////////////////////////////////
+// check email
+/////////////////////////////////////////////////////////////////////////
+if (isset($_POST['check_email']) && $_POST['check_email'] == 'Submit')
+{	
+	if ( ! $_POST['email'] )
+		$err[] = 'Please enter your email address';
+
+	if ( ! count($err) && ! check_email($_POST['email']) )
+		$err[] = 'Your email is not valid';
+	
+	if ( ! count($err) )
+	{
+		// check email
+		$_POST['email'] = mysql_real_escape_string($_POST['email']);
 		
-	</head>
-	<body>
-	<div id="container">
-	<div id="cover"><img src="assets/images/spacer.gif" width="10" height="5" alt="*" /></div>
-	<div id="margin">&nbsp;</div>
-		<div id="nav">
-			<div id="navitem0"><a class="navitem" href="index.html">Home</a></div>
-			<div id="navitem1"><a class="navitem" href="project.html">The Project</a></div>
-			<div id="navitem2"><a class="navitem" href="programmes.html">Programmes</a></div>
-			<div id="navitem3"><a class="navitem" href="project_timeline.html">Project Timeline</a></div>
-			<div id="navitem4"><a class="navitem active" href="news_events.html">News &amp; Events</a></div>
-			<div id="navitem5"><a class="navitem" href="get_involved.html">Get involved</a></div>
-			<div id="navitem6"><a class="navitem" href="fundraising.html">Fundraising</a></div>
-			<div id="navitem7"><a class="navitem" href="contact_us.html">Contact us</a></div>
-		</div>
-		<div id="top">
-			<div id="logo"><a href="index.html"><img src="assets/images/IBS_logo.gif" width="170" height="140" alt="IBS Logo" /></a></div>
-			<div id="image"><img class="mainImage" src="assets/images/image_news_events.jpg" width="750" height="140" alt="Main Image" /></div>
-		</div>	
-		<div id="main">
-			<div id="menuleft">
-				<a class="navitem" href="news_events.html">Launch of IBS</a>
-				<a class="navitem" href="news_events_upcoming.html">Upcoming Events</a>
-				<a class="navitem"><strong>Click on the links below:</strong></a>
-				<a class="navitem" href="JavaScript:CI('01')">01 Fundraising (selected)</a>
-				<a class="navitem" href="JavaScript:CI('02')">02 Fundraising (lighter)</a>
-				<a class="navitem" href="JavaScript:CI('03')">03 News &amp; Events (expensive)</a>
-				<a class="navitem" href="JavaScript:CI('04')">04 News &amp; Events</a>
-				<a class="navitem" href="JavaScript:CI('05')">05 News &amp; Events</a>
-				<a class="navitem" href="JavaScript:CI('06')">06 News &amp; Events</a>
-				<a class="navitem" href="JavaScript:CI('07')">07 News &amp; Events</a>
-				<a class="navitem" href="JavaScript:CI('08')">08 News &amp; Events</a>
-				<a class="navitem" href="JavaScript:CI('09')">09 News &amp; Events</a>
-				<a class="navitem" href="JavaScript:CI('10')">10 Random (free)</a>
-			</div>
-			<div id="content">
-				<div id="title">
-					<div class="text">New images for News &amp; Events / Fundraising pages</div>
-				</div>
-				<div id="body">
-					<p class="intitle">Launch of the Iranian Business School Project</p>
-					<p>The Iranian Business School (IBS) Project was formally launched at a fundraising event held in London on 16 January 2009 at the Jumeirah Carlton Hotel. The event was organised and managed by ISM Management, the UK registered charity, which has been formed to undertake global fundraising in support of the Project.</p>
-					<p>The evening introduced the Project and its aim of establishing a world class business school in Iran.  Addressing guests, Rouzbeh Pirouz and Roshanak Khalili-Dwyer, among the Project's founders, highlighted IBS's mission and the need for such an entity within Iran today. Joining them were Michael Hay, Professor of Management and Entrepreneurship at the London Business School, who is acting as Special Advisor to the Project, and the evening's Guest of Honour, Anousheh Ansari, the first private female and only Iranian space explorer.</p>
-					<p>In support of the Project's funding needs, the evening also saw the auctioning of a special work of art from renowned Iranian artist Parviz Tanavoli, who generously donated his "Standing Heech". The auction was presided over by Edward Gibbs, Director of Islamic Art at Sotheby's, who did a remarkable job of encouraging guests to make generous bids in support of the Project.</p>
-				</div>
-			</div>
-			<div id="menuright">
-				<div class="quicklinks">Quick Links</div>
-				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_3_August_2009.pdf">Quarterly Newsletter, Issue 3, August 2009</a>
-				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_2_April_2009.pdf">Quarterly Newsletter, Issue 2, April 2009</a>
-				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_1_January_2009.pdf">Quarterly Newsletter, Issue 1, January 2009</a>
-				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Brochure.pdf">IBS Project Brochure</a>
-			</div>
-		</div>
-	</div>
-	<div id="footer"><div class="text">Copyright &copy; 2009 Iranian Business School Project</div></div>
-	<script type="text/javascript">Cufon.now();</script>
-	<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-1689775-7");
-pageTracker._trackPageview();
-} catch(err) {}</script>
-	</body>
-</html>
+		// Escaping all input data
+		$row = mysql_fetch_assoc(mysql_query("SELECT email, country FROM $db_table_community WHERE email='{$_POST['email']}'"));
+
+		if ($row['email'])
+			$flag = 'email_ok';
+		else
+			$flag = 'email_new';
+			
+		// check country
+		if ( ($row['country'] == 'US') || ($row['country'] == 'CA') )
+		{
+			if (isset($row['email']))
+				$flag = 'forbidden';
+			else
+				$flag = 'forbidden'; //forbidden_new
+		}
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////
+// check registration
+/////////////////////////////////////////////////////////////////////////
+if (isset($_POST['check_registration']) && $_POST['check_registration'] == 'Submit')
+{
+	$flag = 'email_new';
+
+	if ( ! $_POST['title'] )
+		$err[] = 'Please enter your title';
+	
+	if ( ! $_POST['forename'] )
+		$err[] = 'Please enter your forename';
+
+	if ( ! $_POST['surname'] )
+		$err[] = 'Please enter your surname';
+
+	if ( ! $_POST['country'] )
+		$err[] = 'Please enter your country';
+
+	if ( ! $_POST['email'] )
+	{
+		$err = array();
+		$err[] = 'There was a problem. Please re-enter your email address';
+		$flag = 'start';
+	}
+
+	if ( ! count($err) )
+	{
+		$flag = 'reg_ok';
+		
+		$_POST['title'] = mysql_real_escape_string($_POST['title']);
+		$_POST['forename'] = mysql_real_escape_string($_POST['forename']);
+		$_POST['surname'] = mysql_real_escape_string($_POST['surname']);
+		$_POST['country'] = mysql_real_escape_string($_POST['country']);
+		$_POST['email'] = mysql_real_escape_string($_POST['email']);
+		
+		// check to see if record already exists
+		if (isset($_POST['email']))
+		{
+			$row = mysql_fetch_assoc(mysql_query("SELECT * FROM $db_table_community WHERE email='{$_POST['email']}'"));
+		}
+		
+		if (isset($row['email']))
+		{
+			// update
+			mysql_query("	UPDATE $db_table_community SET
+
+								title = '".$_POST['title']."',
+								forename = '".$_POST['forename']."',
+								surname = '".$_POST['surname']."',
+								country = '".$_POST['country']."',
+								regIP = '".$_SERVER['REMOTE_ADDR']."'
+
+								WHERE email = '".$_POST['email']."'
+
+						");
+			// check country
+			if ( ($_POST['country'] == 'US') || ($_POST['country'] == 'CA') )
+					$flag = 'forbidden';
+
+		}
+		else
+		{
+			// insert
+			mysql_query("	INSERT INTO $db_table_community(dt, email, title, forename, surname, country, regIP)
+
+							VALUES(
+							
+								NOW(),
+								'".$_POST['email']."',
+								'".$_POST['title']."',
+								'".$_POST['forename']."',
+								'".$_POST['surname']."',
+								'".$_POST['country']."',
+								'".$_SERVER['REMOTE_ADDR']."'
+							
+						)");
+			// check country
+			if ( ($_POST['country'] == 'US') || ($_POST['country'] == 'CA') )
+				$flag = 'forbidden'; //forbidden_new
+
+		}
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////
+// check edit registration
+/////////////////////////////////////////////////////////////////////////
+if (isset($_POST['check_edit']) && $_POST['check_edit'] == 'Submit')
+{
+	$flag = 'edit';
+}
+
+?>
+<?php
+
+$this_nav = 6;
+require_once('include/html_head.php');
+
+switch($flag)
+{
+	case 'start':
+		require_once('include/donations_01_email.php');
+		break;
+	case 'email_new':
+		require_once('include/donations_02_reg.php');
+		break;
+	case 'edit':
+		require_once('include/donations_02_reg.php');
+		break;
+	case 'email_ok':
+		require_once('include/donations_03_donate.php');
+		break;
+	case 'reg_ok':
+		require_once('include/donations_03_donate.php');
+		break;
+	case 'forbidden':
+		require_once('include/donations_03_donate.php');
+		break;
+}
+
+require_once('include/html_tail.php');
+
+?>
