b1513dc9e0b92f1572b398c545b2915f5df8fec7
Babak Hatmian <babak.hatamian@imagination.com>
2009-12-28 16:26:22 +0000

    removed subscribe from tickets_buy.php

--- tickets_buy.php
+++ tickets_buy.php
@@ -151,21 +151,11 @@
 			<div id="menuright">
 				<div class="quicklinks">Quick Links</div>
 				<a class="navitem" href="./news_events_upcoming.html">Upcoming Events</a>
+				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_4_December_2009.pdf">Quarterly Newsletter, Issue 4, Dec. 2009</a>
 				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_3_August_2009.pdf">Quarterly Newsletter, Issue 3, August 2009</a>
 				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_2_April_2009.pdf">Quarterly Newsletter, Issue 2, April 2009</a>
 				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_1_January_2009.pdf">Quarterly Newsletter, Issue 1, January 2009</a>
 				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Brochure.pdf">IBS Project Brochure</a>
-				<div class="quicklinks">Subscribe to Newsletter</div>
-				<div id="subscribe">
-					<form id="subscriptionform" action="">
-						<p><input type="text" name="emailAddress" id="emailAddress" value="Enter email address" size="27" />
-						&nbsp;<a href="#" id="submit" class="button"><img src="assets/images/go.gif" alt="GO" /></a>
-						</p>
-					</form>
-					<div id="error"></div>
-					<div id="success">Your email address is now registered.<br /><br />You will start receiving our newsletter shortly.</div>
-				</div>
-				<!--<a class="navitem" href="#">Unsubsribe</a>-->
 			</div>
 		</div>
 	</div>
