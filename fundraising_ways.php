e4ccf6e7393c4bb028584a70763e5ad8838285d4
Babak Hatmian <babak.hatamian@imagination.com>
2010-03-22 18:54:24 +0000

    added donate online button to fundraising pages

--- fundraising_ways.php
+++ fundraising_ways.php
@@ -6,10 +6,15 @@
 		<div id="menuleft">
 			<a class="navitem" href="fundraising.php">Giving To IBS</a>
 			<a class="navitem active" href="fundraising_ways.php">Ways to Give</a>
+			<a class="navitem" href="#" id="donate_start"><img src="assets/images/bullet.gif" width="10" height="13" alt="bullet" />Donate online</a>
 			<a class="navitem" href="fundraising_ways.php#gifts"><img src="assets/images/bullet.gif" width="10" height="13" alt="bullet" />Cheque, Credit &amp; Cash Gifts</a>
 			<a class="navitem" href="fundraising_ways.php#scheduled"><img src="assets/images/bullet.gif" width="10" height="13" alt="bullet" />Scheduled Payments</a>
 			<a class="navitem" href="fundraising_ways.php#pledges"><img src="assets/images/bullet.gif" width="10" height="13" alt="bullet" />Pledges</a>
 			<a class="navitem" href="fundraising_ways.php#memorial"><img src="assets/images/bullet.gif" width="10" height="13" alt="bullet" />Memorial Gifts</a>
+			<form id="main_form" class="form_register" action="register.php" method="post">
+				<input type="hidden" id="page_flag" name="page_flag" value="">
+				<input type="hidden" id="sys_flag" name="sys_flag" value="" />
+			</form>
 		</div>
 		<div id="content">
 			<div id="title">
