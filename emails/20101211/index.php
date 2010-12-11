3eab362c10399d40a092353ca8b06122467f63aa
purplemass <purplemass@gmail.com>
2010-12-11 15:37:39 +0000

    htmml email for sponsorship

---
+++ emails/20101211/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <title>IBS Email 20101211</title>
+</head>
+<body style="margin: 10px; padding: 0;">
+<table width="600" cellpadding="0" cellspacing="0" border="0" style="font-family: Verdana; font-size: 12px; color: #1F2126; text-align: left;">
+	<tr>
+		<td style="background-color: #005d8d;">&nbsp;</td>
+		<td style="background-color: #005d8d;">
+			<span style="color: #FFFFFF; font-weight: bold; display: block; margin: 10px 0 0 5px; font-size: 2.0em; display: block;">Annual IBS Fundraising Event</span>
+			<span style="color: #FFFFFF; display: block; margin: 5px 0 10px 5px; font-size: 1.7em;">Sponsorship Opportunities</span>
+		</td>
+		<td><img src="http://www.ibsproject.org/emails/20101211/images/IBS_logo.gif" alt="IBS Logo" width="100" height="110" /></td>
+	</tr>
+	<tr>
+		<td style="line-height:4px; background-color: #5bb6d5;"><img style="display:inline" src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="15" height="4" alt="*" /></td>
+		<td style="line-height:4px; background-color: #5bb6d5;"><img style="display:inline" src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="485" height="4" alt="*" /></td>
+		<td style="line-height:4px;"><img style="display:inline" src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="100" height="4" alt="*" /></td>
+	</tr>
+</table>
+<table width="600" cellpadding="0" cellspacing="0" border="0" style="font-family: Verdana; font-size: 12px; color: #1F2126; text-align: justify;">
+	<tr>
+		<td style="line-height:10px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="15" height="10" alt="*" /></td>
+		<td style="line-height:10px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="270" height="10" alt="*" /></td>
+		<td style="line-height:10px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="25" height="10" alt="*" /></td>
+		<td style="line-height:10px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="270" height="10" alt="*" /></td>
+		<td style="line-height:10px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="15" height="10" alt="*" /></td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td valign="top">
+			<p style="margin-bottom: 1em; line-height: 1.3;">The Iranian Business School (IBS) Project&apos;s Annual Fundraising Event will take place on <strong>4th February 2011</strong> at the Ballroom of <strong>The Dorchester Hotel</strong> in London and will provide an ideal opportunity to join together with other members of the Iranian community to help support the progress of an independent, world-class business school in Iran.</p>
+			<p style="margin-bottom: 1em; line-height: 1.3;">Featuring a keynote speech by <strong>Dr. Eslambolchi, Chairman and CEO of consulting firm 2020 Venture Partners</strong>, and a publicly acclaimed global thought leader and &ldquo;technological genius&rdquo;, our event will also include an exclusive art auction of top works by renowned Iranian artists. Previous keynote speakers included Anousheh Ansari in 2009, the world&apos;s first female private space explorer, and Omid Kordestani in 2010, from the Office of Founders at Google.</p>
+			<p style="margin-bottom: 1em; line-height: 1.3;">
+				<span style="display: block; color: #346794; font-size: 1.1em; font-weight: bold; margin-bottom: 2px;">About the IBS Project</span>
+				The IBS Project is a not-for-profit venture spearheaded by a group of philanthropists from the business community of the Iranian Diaspora and supported by leading private sector figures within Iran. Together, these individuals have been inspired to engage in a movement to bring opportunity to Iran, a nation brimming with unrealised potential. Educated,ambitious and enterprising,young Iranian citizens possess the same qualities that have placed thousands of their peers abroad amongst a recognised category of the international business elite. What differentiates them is access to the facilities that can nurture their talents, fortify their knowledge and provide the necessary proficiencies to participate in the global economy.
+			</p>
+			<span style="display: block; color: #346794; font-size: 1.1em; font-weight: bold;">Sponsorship Packages</span>
+		</td>
+		<td>&nbsp;</td>
+		<td valign="top">
+			<p style="margin-bottom: 1em; line-height: 1.3;">IBS&apos;s previous Fundraising Event (held in February 2010), was attended by more than 300 Iranian business leaders and members of the community and we fully expect that this year&apos;s event will receive an even greater degree of support from the numerous individuals, institutions and donors that have helped to drive the Project forward over the course of the past year.</p>
+			<p style="margin-bottom: 1em; line-height: 1.3;">As a philanthropically-motivated project, we rely upon the cooperation of like-minded individuals wishing to help in the development of the next generation of business leaders in Iran, and would be grateful for your support and participation.</p>
+			<p style="margin-bottom: 1em; line-height: 1.3;">
+				<span style="display: block; color: #346794; font-size: 1.1em; font-weight: bold; margin-bottom: 2px;">Sponsorship Programme</span>
+				We are currently seeking sponsors for the forthcoming 2011 Fundraising Event.
+			</p>
+			<p style="margin-bottom: 1em; line-height: 1.3;">Please find below details of our sponsorship packages designed for corporates and individuals. These packages have been formulated to provide participating sponsors with optimal exposure to a targeted global audience, as well as the opportunity to support the vision of bringing world-class business education to Iran.</p>
+			<p style="margin-bottom: 1em; line-height: 1.3;">We thank you in advance for your consideration and look forward to discussing the Iranian Business School Project and our upcoming event with you at your earliest convenience. In the meantime, should you wish to find out more about the project, please visit our website at <a href="http://www.ibsproject.org" style="color: #1F2126; font-weight: bold; text-decoration: underline;">www.ibsproject.org</a> or contact us on <a href="mailto:events@ibsproject.org" style="color: #1F2126; font-weight: bold; text-decoration: underline;">events@ibsproject.org</a></p>
+		</td>
+		<td>&nbsp;</td>
+	</tr>
+</table>
+<table width="600" cellpadding="2" cellspacing="0" border="0" style="font-family: Verdana; font-size: 12px; color: #1F2126; text-align: left;">
+	<tr style="color: #FFFFFF;">
+		<td style="line-height:1px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="6" height="1" alt="*" /></td>
+		<td style="line-height:1px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="146" height="1" alt="*" /></td>
+		<td style="line-height:1px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="111" height="1" alt="*" /></td>
+		<td style="line-height:1px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="101" height="1" alt="*" /></td>
+		<td style="line-height:1px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="101" height="1" alt="*" /></td>
+		<td style="line-height:1px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="101" height="1" alt="*" /></td>
+		<td style="line-height:1px;"><img src="http://www.ibsproject.org/emails/20101211/images/spacer.gif" width="6" height="1" alt="*" /></td>
+	</tr>
+	<tr style="height: 26px; color: #FFFFFF;">
+		<td>&nbsp;</td>
+		<td style="background-color: #005d8d;">&nbsp;</td>
+		<td style="background-color: #005d8d;">Platinum Sponsor</td>
+		<td style="background-color: #005d8d;">Gold Sponsor</td>
+		<td style="background-color: #005d8d;">Silver Sponsor</td>
+		<td style="background-color: #005d8d;">Bronze Sponsor</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr style="height: 26px;">
+		<td>&nbsp;</td>
+		<td style="background-color: #e8e9ec">Cost</td>
+		<td style="background-color: #e8e9ec">&pound;10,000</td>
+		<td style="background-color: #e8e9ec">&pound;4,000</td>
+		<td style="background-color: #e8e9ec">&pound;2,000</td>
+		<td style="background-color: #e8e9ec">&pound;500</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr style="height: 54px;">
+		<td>&nbsp;</td>
+		<td>IBS Website until<br />End Feb 2012<br />(Event Section)</td>
+		<td>Top Logo &amp;<br />Company<br />Details</td>
+		<td>Logo &amp;<br />Company<br />Details</td>
+		<td>Logo &amp;<br />Company<br />Details</td>
+		<td>(one line)</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr style="height: 26px;">
+		<td>&nbsp;</td>
+		<td style="background-color: #e8e9ec">Event Tickets</td>
+		<td style="background-color: #e8e9ec">Full Table (12)</td>
+		<td style="background-color: #e8e9ec">Half Table (6)</td>
+		<td style="background-color: #e8e9ec">3</td>
+		<td style="background-color: #e8e9ec">1</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr style="height: 41px;">
+		<td>&nbsp;</td>
+		<td>Name/Logo Displayed<br />on Tickets</td>
+		<td>Yes</td>
+		<td>-</td>
+		<td>-</td>
+		<td>-</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr style="height: 54px;">
+		<td>&nbsp;</td>
+		<td style="background-color: #e8e9ec">Name/Logo Displayed<br />on Evening Programme</td>
+		<td style="background-color: #e8e9ec">2 Full Pages</td>
+		<td style="background-color: #e8e9ec">1 Full Page</td>
+		<td style="background-color: #e8e9ec">Half Page</td>
+		<td style="background-color: #e8e9ec">Mentioned on the Thank You page</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr style="height: 41px;">
+		<td>&nbsp;</td>
+		<td>Newsletter Coverage<br />(To 3000+ Subscribers)</td>
+		<td>Yes</td>
+		<td>Yes</td>
+		<td>Yes</td>
+		<td>Yes</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr style="height: 26px;">
+		<td>&nbsp;</td>
+		<td style="background-color: #e8e9ec">Free Raffle Tickets</td>
+		<td style="background-color: #e8e9ec">20</td>
+		<td style="background-color: #e8e9ec">10</td>
+		<td style="background-color: #e8e9ec">5</td>
+		<td style="background-color: #e8e9ec">1</td>
+		<td>&nbsp;</td>
+	</tr>
+	<tr style="height: 26px;">
+		<td>&nbsp;</td>
+		<td colspan="5">
+			<br /><span style="display: block; color: #346794; font-size: 1.1em; font-weight: bold;">For further info, please contact us on <a href="mailto:events@ibsproject.org" style="color: #4c749e; font-weight: bold; text-decoration: underline;">events@ibsproject.org</a></span>
+			<br />
+		</td>
+		<td>&nbsp;</td>
+	</tr>
+</table>
+</body>
+</html>
