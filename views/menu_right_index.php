e38fcc404d8e6fa32dfe253fb1a8d17eaeeccd13
purplemass <purplemass@gmail.com>
2013-04-12 15:12:36 +0100

    took out brochure + typo fixed

--- views/menu_right_index.php
+++ views/menu_right_index.php
@@ -19,7 +19,7 @@
 <!-- THIS WAS DONE A LONG TIME AGO
 -->
 			<a class="navitem" href="./news_events_upcoming.php">Upcoming Events</a>
-<!-- REMOVED newsletters
+<!-- REMOVED newsletters & brochure
 			<a class="navitem" rel="new" href="./_downloads	/IBS_Project_Newsletter_Issue_9_April_2011.pdf">Newsletter, Issue 9, April 2011</a>
 			<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_7_and_8_Jan_2011.pdf">Newsletter, Issues 7/8, January 2011</a>
 			<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_6_June_2010.pdf">Newsletter, Issue 6, June 2010</a>
@@ -31,8 +31,8 @@
 				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_2_April_2009.pdf">Newsletter, Issue 2, April 2009</a>
 				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_1_January_2009.pdf">Newsletter, Issue 1, January 2009</a>
 			</div>
--->
 			<a class="navitem" rel="new" href="./_downloads/IBS_Project_Brochure.pdf">IBS Project Brochure</a>
+-->
 			<div class="quicklinks">Subscribe to Newsletter</div>
 			<div id="subscribe">
 				<form id="newsletter_form" action="">
@@ -43,5 +43,5 @@
 				<div id="error"></div>
 				<div id="success">Your email address is now registered.<br /><br />You will start receiving our newsletters shortly.</div>
 			</div>
-			<!--<a class="navitem" href="#">Unsubsribe</a>-->
+			<!-- <a class="navitem" href="#">Unsubsribe</a> -->
 		</div>
--- views/menu_right_links.php
+++ views/menu_right_links.php
@@ -8,7 +8,7 @@
 		<div id="menuright">
 			<div class="quicklinks">Quick Links</div>
 			<a class="navitem" href="./news_events_upcoming.php">Upcoming Events</a>
-<!-- REMOVED newsletters
+<!-- REMOVED newsletters & brochure
 			<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_9_April_2011.pdf">Newsletter, Issue 9, April 2011</a>
 			<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_7_and_8_Jan_2011.pdf">Newsletter, Issues 7/8, January 2011</a>
 			<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_6_June_2010.pdf">Newsletter, Issue 6, June 2010</a>
@@ -20,8 +20,8 @@
 				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_2_April_2009.pdf">Newsletter, Issue 2, April 2009</a>
 				<a class="navitem" rel="new" href="./_downloads/IBS_Project_Newsletter_Issue_1_January_2009.pdf">Newsletter, Issue 1, January 2009</a>
 			</div>
--->
 			<a class="navitem" rel="new" href="./_downloads/IBS_Project_Brochure.pdf">IBS Project Brochure</a>
+-->
 <!--
 			<div class="quicklinks">Subscribe to Newsletter</div>
 			<div id="subscribe">
@@ -34,5 +34,5 @@
 				<div id="success">Your email address is now registered.<br /><br />You will start receiving our newsletters shortly.</div>
 			</div>
 -->
-			<!--<a class="navitem" href="#">Unsubsribe</a>-->
+			<!-- <a class="navitem" href="#">Unsubsribe</a> -->
 		</div>
